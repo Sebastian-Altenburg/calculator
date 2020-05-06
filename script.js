94ccf4a27dafdabe377a3f97744f3cbf46a6bbbc
sebastian-altenburg <saltenburg90@gmail.com>
2020-05-06 16:05:23 +0200

    switched approach, works with multiple digits now

--- script.js
+++ script.js
@@ -1,352 +1,142 @@
-/////////Global Variables
+function Calculator(previousOperandText, currentOperandText){
+    this.previousOperandText = previousOperandText;
+    this.currentOperandText = currentOperandText;
 
-let firstValue = "";
-let secValue = "";
-let solution;
-
-/////////////////////////
-
-function add(num1, num2){
-    solution = parseInt(num1) + parseInt(num2);
-    strSolution = solution.toString();
-    document.getElementsByClassName("display")[0].value = strSolution;  
-}
-function subtract(num1, num2){
-    solution = parseInt(num1) - parseInt(num2);
-    strSolution = solution.toString();
-    document.getElementsByClassName("display")[0].value = strSolution;
-}
-function multiply(num1,num2){
-    solution = parseInt(num1) * parseInt(num2);
-    strSolution = solution.toString();
-    document.getElementsByClassName("display")[0].value = strSolution;
-}
-function divide(num1,num2){
-    solution = parseInt(num1) / parseInt(num2);
-    strSolution = solution.toString();
-    document.getElementsByClassName("display")[0].value = strSolution;
-}
-
-function operate(mathOperator, num1, num2){
+    this.clear = function(){
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+    this.delete = function(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+    this.appendNumber = function(number){   
+        this.currentOperand = this.currentOperand.toString() + number;
+    }
+    this.chooseOperation = function(operation){
+        if(this.currentOperand === "") return;
+        if (this.previousOperand !== "") {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+    }
     
-    if (mathOperator == "plus") {
-        add(num1, num2);
+    this.compute = function(){
+        let compulation;
+        let prev = parseInt(this.previousOperand);
+        let current = parseInt(this.currentOperand);
+
+        if(isNaN(prev) || isNaN(current)) return;
+
+        if(this.operation === "+") compulation = prev + current;
+        else if(this.operation === "-") compulation = prev - current;
+        else if(this.operation === "*") compulation = prev * current;
+        else if(this.operation === "/") compulation = prev / current;
+        else return;
+
+        this.currentOperand = compulation;
+        this.operation = undefined;
+        this.previousOperand = "";
+
     }
-    else if (mathOperator == "minus") {
-        subtract(num1, num2);
+    this.updateDisplay = function(){
+        this.currentOperandText.innerText = this.currentOperand;
+        this.previousOperandText.innerText = this.previousOperand;
     }
-    else if (mathOperator == "multi") {
-        multiply(num1, num2);
-    }
-    else if (mathOperator == "dividing") {
-        divide(num1, num2);
-    }
+
 }
 
-
-
+///////////////////////////////////////////////////
 let parentDiv = document.querySelector("div");
-parentDiv.childNodes[0].classList.add("display");
+let buttons;
 
-for (let i = 0; i < 17; i++) {
-    let childDiv = document.createElement("button");
-    // childDiv.innerHTML = "BUTTON";
-    parentDiv.appendChild(childDiv);
+for (let i = 0; i < 18; i++) {
+    buttons = document.createElement("button");
+    parentDiv.appendChild(buttons);
 }
 
-
-
-
-firstNumber();
-function firstNumber(){
-    
-    
-
-    parentDiv.childNodes[1].innerHTML = "+";
-    parentDiv.childNodes[1].addEventListener("click",()=>{
-
-        document.querySelector(".display").value += "+";
-        secondNum("plus", firstValue);
-    });
-    
-    parentDiv.childNodes[2].innerHTML = "7";
-    parentDiv.childNodes[2].addEventListener("click", ()=> {
-        firstValue += 7;
-        document.querySelector(".display").value = firstValue;
-        
-    });
-    
-    parentDiv.childNodes[3].innerHTML = "8";
-    parentDiv.childNodes[3].addEventListener("click", ()=> {
-        firstValue += 8;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[4].innerHTML = "9";
-    parentDiv.childNodes[4].addEventListener("click", ()=> {
-        firstValue += 9;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[5].innerHTML = "-";
-    parentDiv.childNodes[5].addEventListener("click",()=>{
-        document.querySelector(".display").value += "-";
-        secondNum("minus", firstValue);
-    });
-    
-    parentDiv.childNodes[6].innerHTML = "4";
-    parentDiv.childNodes[6].addEventListener("click", ()=> {
-        firstValue += 4;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[7].innerHTML = "5";
-    parentDiv.childNodes[7].addEventListener("click", ()=> {
-        firstValue += 5;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[8].innerHTML = "6";
-    parentDiv.childNodes[8].addEventListener("click", ()=> {
-        firstValue += 6;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[9].innerHTML = "*";
-    parentDiv.childNodes[9].addEventListener("click",()=>{
-        document.querySelector(".display").value += "*";
-        secondNum("multi", firstValue);
-    });
-    
-    parentDiv.childNodes[10].innerHTML = "1";
-    parentDiv.childNodes[10].addEventListener("click", ()=> {
-        firstValue += 1;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[11].innerHTML = "2";
-    parentDiv.childNodes[11].addEventListener("click", ()=> {
-        firstValue += 2;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[12].innerHTML = "3";
-    parentDiv.childNodes[12].addEventListener("click", ()=> {
-        firstValue += 3;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[13].innerHTML = "/";
-    parentDiv.childNodes[13].addEventListener("click",()=>{
-        document.querySelector(".display").value += "/";
-        secondNum("dividing", firstValue);
-    });
-    
-    parentDiv.childNodes[14].innerHTML = "0";
-    parentDiv.childNodes[14].addEventListener("click", ()=> {
-        firstValue += 0;
-        document.querySelector(".display").value = firstValue;
-    });
-    parentDiv.childNodes[15].innerHTML = " ";
-    parentDiv.childNodes[16].innerHTML = "CLEAR";
-    parentDiv.childNodes[16].addEventListener("click", ()=>{
-        location.reload();
-    });
-    parentDiv.childNodes[17].innerHTML = "=";
-    parentDiv.childNodes[17].addEventListener("click",()=>{
-        document.querySelector(".display").value = firstValue;
-    });
-
+for (let i = 0; i<parentDiv.children.length + 1; i++) {
+    if(i == 5 || i == 9 || i == 13 || i == 17){
+        parentDiv.childNodes[i].classList.add("operation");
+    }
+    else if(i==6||i ==7||i==8||i==10||i==11||i==12||i==14||i==15||i==16||i==19) {
+        parentDiv.childNodes[i].classList.add("numClass");
+    }   
+    else if (i == 4) parentDiv.childNodes[i].classList.add("delete");
 }
 
+////////////////////////////////////////////////////////
+parentDiv.childNodes[3].innerHTML = "AC";
+parentDiv.childNodes[3].classList.add("allClear");
 
-//////////////////////////////////////////
-function secondNum(operator, firstValue){
-    
-    // parentDiv.childNodes[0].classList.add("display");
+parentDiv.childNodes[4].innerHTML = "DEL";
+parentDiv.childNodes[5].innerHTML = "+";
+parentDiv.childNodes[6].innerHTML = "1";
+parentDiv.childNodes[7].innerHTML = "2";
+parentDiv.childNodes[8].innerHTML = "3";
+parentDiv.childNodes[9].innerHTML = "*";
+parentDiv.childNodes[10].innerHTML = "4";
+parentDiv.childNodes[11].innerHTML = "5";
+parentDiv.childNodes[12].innerHTML = "6";
+parentDiv.childNodes[13].innerHTML = "/";
+parentDiv.childNodes[14].innerHTML = "7";
+parentDiv.childNodes[15].innerHTML = "8";
+parentDiv.childNodes[16].innerHTML = "9";
+parentDiv.childNodes[17].innerHTML = "-";
+parentDiv.childNodes[18].innerHTML = "";
+parentDiv.childNodes[19].innerHTML = "0";
 
-    // parentDiv.childNodes[1].innerHTML = "+";
-    // parentDiv.childNodes[1].addEventListener("click",()=>{});
-    
-    parentDiv.childNodes[2].innerHTML = "7";
-    parentDiv.childNodes[2].addEventListener("click", ()=> {
-        secValue += 7;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-        
-    });
-    
-    parentDiv.childNodes[3].innerHTML = "8";
-    parentDiv.childNodes[3].addEventListener("click", ()=> {
-        secValue += 8;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    parentDiv.childNodes[4].innerHTML = "9";
-    parentDiv.childNodes[4].addEventListener("click", ()=> {
-        secValue += 9;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    // parentDiv.childNodes[5].innerHTML = "-";
-    // parentDiv.childNodes[5].addEventListener("click",()=>{});
-    
-    parentDiv.childNodes[6].innerHTML = "4";
-    parentDiv.childNodes[6].addEventListener("click", ()=> {
-        secValue += 4;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    parentDiv.childNodes[7].innerHTML = "5";
-    parentDiv.childNodes[7].addEventListener("click", ()=> {
-        secValue += 5;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    parentDiv.childNodes[8].innerHTML = "6";
-    parentDiv.childNodes[8].addEventListener("click", ()=> {
-        secValue += 6;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    // parentDiv.childNodes[9].innerHTML = "*";
-    // parentDiv.childNodes[9].addEventListener("click",()=>{});
-    
-    parentDiv.childNodes[10].innerHTML = "1";
-    parentDiv.childNodes[10].addEventListener("click", ()=> {
-        secValue += 1;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    parentDiv.childNodes[11].innerHTML = "2";
-    parentDiv.childNodes[11].addEventListener("click", ()=> {
-        secValue += 2;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    parentDiv.childNodes[12].innerHTML = "3";
-    parentDiv.childNodes[12].addEventListener("click", ()=> {
-        secValue += 3;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    // parentDiv.childNodes[13].innerHTML = "/";
-    // parentDiv.childNodes[13].addEventListener("click",()=>{});
-    
-    parentDiv.childNodes[14].innerHTML = "0";
-    parentDiv.childNodes[14].addEventListener("click", ()=> {
-        secValue += 0;
-        if(operator == "plus"){
-            document.querySelector(".display").value = firstValue + "+" + secValue;
-        }
-        else if (operator == "minus") {
-            document.querySelector(".display").value = firstValue + "-" + secValue;
-        }
-        else if (operator == "multi") {
-            document.querySelector(".display").value = firstValue + "*" + secValue;
-        }
-        else if (operator == "dividing") {
-            document.querySelector(".display").value = firstValue + "/" + secValue;
-        }
-    });
-    
-    parentDiv.childNodes[16].innerHTML = "CLEAR";
-    parentDiv.childNodes[16].addEventListener("click", ()=>{
-        location.reload();
-    });
-    parentDiv.childNodes[17].innerHTML = "=";
-    parentDiv.childNodes[17].addEventListener("click",()=>{
-        operate(operator, firstValue, secValue);
-    });
+parentDiv.childNodes[20].innerHTML = "=";
+parentDiv.childNodes[20].classList.add("equals");
+////////////////////////////////////////////////////////
 
 
-    parentDiv.childNodes[15].innerHTML = " ";
+let numberButtons = document.querySelectorAll(".numClass");
+let operationButtons = document.querySelectorAll(".operation");
 
-}
+let equalsButton = document.querySelector(".equals");
+let deleteButton = document.querySelector(".delete");
+let allClearButton = document.querySelector(".allClear");
+
+let previousOperandText = document.querySelector(".previous");
+let currentOperandText = document.querySelector(".current");
 
 
+
+let calculator = new Calculator(previousOperandText, currentOperandText);
+calculator.clear();
+
+allClearButton.addEventListener("click",()=>{
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+numberButtons.forEach((button=>{
+    button.addEventListener("click", ()=>{
+        calculator.appendNumber(button.innerText.toString());  
+        calculator.updateDisplay();
+    });
+}));
+
+
+operationButtons.forEach((button=>{
+    button.addEventListener("click", ()=>{
+        calculator.appendNumber(button.innerText.toString()); 
+        calculator.chooseOperation(button.innerText.toString());  
+        calculator.updateDisplay();
+    });
+}));
+
+equalsButton.addEventListener("click", ()=> {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+deleteButton.addEventListener("click", ()=>{
+    calculator.delete();
+    calculator.updateDisplay();
+});
 
 
 
